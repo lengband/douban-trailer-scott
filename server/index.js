1696e7535446fe9ca5b414300a68257eb02c4a64
wangpeng <wangpeng@yunionyun.com>
2018-08-19 16:39:26 +0800

    done

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const Koa = require('koa')
-const { normal } = require('./tpl')
 const app = new Koa()
+const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
+const ejs = require('ejs')
+const pug = require('pug')
 
 app.use(async (ctx, next) => {
   ctx.type = 'text/html; charset=utf-8'
-  ctx.body = normal
+  ctx.body = ejs.render(ejsTpl, {
+    you: 'Scott',
+    me: 'WangPeng'
+  })
 })
 
 app.listen(4455)
