41504d0128f8d9d046283be458d5ad44beccfbb8
wangpeng <wangpeng@yunionyun.com>
2018-09-13 21:51:23 +0800

    fix

--- server/tasks/qiniu.js
+++ server/tasks/qiniu.js
@@ -30,6 +30,15 @@ const uploadToQiniu = async (url, key) => {
     poster: 'https://img3.doubanio.com/view/photo/l_ratio_poster/public/p2529088881.jpg',
     cover: 'https://img3.doubanio.com/img/trailer/medium/2531818052.jpg'
   }]
+  const res = {
+    video: 'http://vt1.doubanio.com/201809032113/743425efac587304f08ee6d8884d09c8/view/movie/M/402350302.mp4',
+    doubanId: '27606054',
+    poster: 'https://img3.doubanio.com/view/photo/l_ratio_poster/public/p2529088881.jpg',
+    cover: 'https://img3.doubanio.com/img/trailer/medium/2531818052.jpg',
+    videoKey: '1h5g4dTLd6APidPKpBvu1.mp4',
+    coverKey: 'R5mVsQInZOOb4SxySWEtU.jpg',
+    posterKey: 'iKgB79HaTjVsFDkCOzmQt.jpg'
+  }
   movies.map(async movie => {
     if (movie.video && !movie.key) {
       try {
