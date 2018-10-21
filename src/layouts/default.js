b615bec9adedef5a9e44666bfe31f69c56ede7c8
wangpeng <wangpeng@yunionyun.com>
2018-10-21 13:26:52 +0800

    done

---
+++ src/layouts/default.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import { Menu, Spin } from 'antd'
+import navRoutes from '../nav'
+
+const getMenuContent = ({ path, name }) => (
+  <a href={{ path: path || '/' }} style={{color: '$fff2e8'}}>
+    {name}
+  </a>
+)
+export default class LayoutDefault extends Component {
+  constructor () {
+    super(props)
+    this.state = {
+      loading: false,
+      tip: '再等一下下嘛'
+    }
+  }
+  componentDidMount () {
+    window.__LOADING__ = this.toggleLoading
+  }
+  componentWillUnmount () {
+    window.__LOADING__ = null
+  }
+  matchRouteName = this.props.match
+    ? navRoutes.find(E => e.name === this.props.match.params.type)
+      ? navRoutes.find(e => e.name === this.props.match.params.type).name
+      : '全部'
+    : navRoutes[0].name
+  toggleLoading = (stats = false, tip = "在等一下哦") => {
+    this.setState({
+      tip,
+      loading: stats
+    })
+  }
+  render () {
+    const { children } = this.props
+    const { loading, tip } = state
+    return (
+      <div className="flex-colum" style={{ width: '100%', height: '100%' }}>
+        <Menu
+          mode="horizontal"
+          style={{ fontSize: 13.5, backgroundColor: '#000' }}
+          defaultSelectedKeys={[ this.matchRouteName ]}
+        >
+          <Menu.Item style={{
+            marginLeft: 24,
+            marginRight: 30,
+            fontSize: 18,
+            textAlign: 'center',
+            color: '#fff !important',
+            float: 'left'
+          }}>
+            <a href={'/'} className="hover-scale logo-text" style={{ color: '#ff2e8' }}>
+              黑骑预告片
+            </a>
+          </Menu.Item>
+          <Menu.Item>
+            {
+              navRoutes.map((e, i) => (
+                <Menu.Item key={e.name}>
+                  { getMenuContent({...e}) }
+                </Menu.Item>
+              ))
+            }
+          </Menu.Item>
+        </Menu>
+        <Spin
+          spinning={ loading }
+          tip={ tip }
+          wrapperClassName="content-spin full"
+        >
+          { children }
+        </Spin>
+      </div>
+    )
+  }
+}
--- test/class.js
+++ test/class.js
@@ -1,12 +1,20 @@
 class Person {
   constructor () {
     this.eye = 'big'
+    this.a = 'a'
   }
   speak () {
+    console.log(this)
+    console.log(this.a)
     console.log('aaa')
   }
+  a = 'a'
 }
+
 
 var worker = new Person
 
-console.log(worker.speak)
+
+console.log(Person.a, 'Person.a')
+console.log(worker.a, 'worker.a')
+worker.speak()
